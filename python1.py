813d73c13c3944e668a6701d59e42bac5b25de55
Maga Moorthy <priyamoorthy350@gmail.com>
2025-10-10 14:24:03 +0530

    update python1.py and python2.py

---
+++ python1.py
@@ -0,0 +1,7 @@
+def multiply(n):
+    if n%7==0:
+        print("yes")
+    else:
+        print("no")
+
+multiply(12)
---
+++ python2.py
@@ -0,0 +1,11 @@
+def numbers(a,b,c):
+    if a>=b and a>=c:
+        print("a")
+    elif b>=a and b>=c:
+        print("b")
+    else:
+        print("c")
+
+numbers(3,56,100)
+
+
